79ed7961eb9598af5ff1ee6641a9d968edd2e9e0
Bea Castro <bea.castro@mail.utoronto.ca>
2023-09-18 09:54:48 -0400

    completing TODO 1

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -4,7 +4,7 @@ public class DataTypes {
     // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
 
-        int s = 0;
+        long s = 0; // because the int datatype has a max value it can store
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
