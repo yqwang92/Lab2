cb31c23c9959d147dc231c5fcee30498d05292ad
Nicolas Dias Martins <nicolasci2018@gmail.com>
2023-09-18 09:56:17 -0400

    fixed todo

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,14 +1,16 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataTypes {
-    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
+    // TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
 
         int s = 0;
         // below is a "foreach" loop which iterates through numbers
-        for (int x : numbers) {
+        for (Integer x: numbers) {
             s += x;
         }
         return s;
     }
 }
+
